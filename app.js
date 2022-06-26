cc853d2cf9a49d02cdcdf9ef33f7cc3c55b0afbe
wkchiuea <103587188+wkchiuea@users.noreply.github.com>
2022-06-26 17:25:05 +0800

    [update] add delete item and custom list

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true}
 const itemSchema = new mongoose.Schema({
   name: String,
 });
-
 const Item = mongoose.model("Item", itemSchema);
 
 const item1 = new Item({
@@ -27,6 +26,14 @@ const item3 = new Item({
   name: "hoho"
 });
 const defaultItems = [item1, item2, item3];
+
+const listSchema = new mongoose.Schema({
+  name: String,
+  items: [itemSchema]
+});
+const List = mongoose.model("List", listSchema);
+
+
 
 app.get("/", function(req, res) {
 
@@ -62,12 +69,46 @@ app.post("/", function(req, res){
 
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.post("/delete", function(req, res) {
+  const checkedItemId = req.body.checkbox;
+
+  Item.findByIdAndRemove(checkedItemId, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/");
+    }
+  });
+
+  // Item.deleteOne({_id: checkItemId}, function(err) {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     console.log("Successfully deleted one item.");
+  //   }
+  // });
+
 });
 
-app.get("/about", function(req, res){
-  res.render("about");
+app.get("/:customListName", function(req, res) {
+  const customListName = req.params.paramName;
+
+  List.findOne({name: customListName}, function(err, foundList) {
+    if (err) {
+      console.log(err);
+    } else if (!foundList) {
+      const list = new List({
+        name: customListName,
+        items: defaultItems
+      });
+    
+      list.save();
+      res.redirect("/" + customListName);
+    } else {
+      res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
+    }
+  });
+
 });
 
 app.listen(3000, function() {
